9ba9026da8f3622229cac6865b33db847c844efb
tarik <sdd@sd.ccs>
2019-09-02 18:07:07 +0200

    frist commit

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Tarik M on 9/2/19.
+//  Copyright © 2019 Tarik M. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["one","two","three"]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK - Tableview Datasource method
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text =  itemArray[indexPath.row]
+        return cell
+    }
+    
+    
+    //MARK - TableView delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+    
+    
+    
+}
+
+
